5490dac86eb2a42a28bc74cc1ef1a0cf1e81d67a
Abdur Rahman <abdurmrahman7@gmail.com>
2024-08-20 18:00:18 +0530

    - Vite Init - pnpm & node version setup + husky setup - Theme Load

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className='h1 text-x-prim'>Hello World</h1>
+      <h1 className='h2'>Hello World</h1>
+      <h1 className='h3'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        x: {
+          // primary
+          prim: {
+            DEFAULT: "#635FC7",
+            light: "#A8A4FF",
+          },
+          red: {
+            DEFAULT: "#EA5555",
+            light: "#FF9898",
+          },
+          slate: {
+            0: "#FFFFFF", // white
+            100: "#F4F7FD",
+            200: "#E4EBFA",
+            300: "#828FA3",
+            400: "#3E3F4E",
+            500: "#2B2C37",
+            600: "#20212C",
+            700: "#000112",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
